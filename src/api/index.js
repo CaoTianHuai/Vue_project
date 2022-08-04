b69e93bdd4143f6dada8f273b30542769a8da8b2
12894253 <1937290258@qq.com>
2022-08-04 11:48:35 +0800

    使用mcokjs模拟轮播图数据,并使用watch和$nextTick优化轮播图

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 //当前模块:API进行统一管理
 import request from "./request";
-
+import mockRequest from './mock'
 //三级联动的接口
 export const reqCategoryList = ()=>request({
   url:'/product/getBaseCategoryList',
   method:'get'
 })
-
+ //获取banner(首页轮播图)
+ export const reqGetBannerList = ()=>mockRequest.get('/banner')
--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,15 +1,20 @@
 //home模块的仓库
-import { reqCategoryList } from "@/api"
+import { reqCategoryList , reqGetBannerList} from "@/api"
 
 //state:仓库存储数据的地方
 const state = {
-  categoryList:[]
+  categoryList:[],
+  //轮播图的数据
+  bannerList:[]
 }
 
 //mutations:修改state的唯一手段
 const mutations = {
   CATEGORYLIST(state,categoryList){
     state.categoryList = categoryList
+  },
+  GETBANNERLIST(state,bannerList){
+    state.bannerList = bannerList
   }
 }
 
@@ -19,6 +24,13 @@ const actions = {
     let result = await reqCategoryList(); 
     if (result.code === 200) {
       commit('CATEGORYLIST',result.data)
+    }
+   },
+   //获取首页轮播图的数据
+   async getBannerList({commit}){
+    let result = await reqGetBannerList();
+    if (result.code == 200) {
+      commit('GETBANNERLIST',result.data)
     }
    }
 }

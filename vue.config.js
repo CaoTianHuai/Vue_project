9935c6597f28df0a8e2a81042f976aede5251e9f
12894253 <1937290258@qq.com>
2022-09-01 10:08:57 +0800

    添加注册校验,前端页面完结

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  productionSourceMap:false,
   //关闭eslint校验
   lintOnSave:false,
   transpileDependencies: true,
